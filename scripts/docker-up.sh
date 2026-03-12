d279cb9078efce62b932c9ce07607ddb3c7ae752
Abhimanyu Singh <abhimanyu4016@gmail.com>
2026-03-12 16:06:13 +0530

    Include minio-init in docker:up for auto bucket creation

--- scripts/docker-up.sh
+++ scripts/docker-up.sh
@@ -232,7 +232,7 @@ LOCAL_DEMO_SEED="$LOCAL_DEMO_SEED_VALUE" \
 AUTH_AUTO_PROVISION_USERS="$AUTH_AUTO_PROVISION_USERS_VALUE" \
 LOCAL_DEMO_CONFIG_PATH="$LOCAL_DEMO_CONFIG_PATH_VALUE" \
 ENV="$ENV_MODE" \
-"${COMPOSE_CMD[@]}" up -d --build --remove-orphans backend frontend mongo mongo-express mailhog mailhog-oauth local-sts
+"${COMPOSE_CMD[@]}" up -d --build --remove-orphans backend frontend mongo mongo-express mailhog mailhog-oauth local-sts minio-init
 wait_for_http_contains "$BACKEND_HEALTH_URL" "\"ready\":true" "backend" 600
 wait_for_http_contains "$FRONTEND_URL" "<html" "frontend" 300
 
